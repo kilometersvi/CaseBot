643f544b0ee9cdfb6a8b699395f622c7face1eba
kilometersvi <milesmilosevich@gmail.com>
2020-04-28 15:24:47 -0700

    userbase interface added

--- userbase.py
+++ userbase.py
@@ -1,13 +1,13 @@
 from sql import db
 from data import data
 
-from mysql.connector import errors
-
 class userbase:
     log = False
     table_twitter = "users_twitter"
     table_reddit = "users_reddit"
 
+    sql_user = "miles"
+    sql_pass = "shrek5"
     '''
     user data:
         id (string) - id
@@ -26,7 +26,11 @@ class userbase:
     '''
 
     def create_usertables(): #only run on new database
-        connection = db.create_connection(data.sql_addr, data.sql_user, data.sql_pass, data.db_name)
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass)
+        db.run(connection,"Create database if not exists "+data.db_name)
+        db.commit(connection)
+        connection.close()
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass, data.db_name)
         db.run(connection,"Create table "+table_twitter+" (id VARCHAR(64) NOT NULL UNIQUE, location MEDIUMINT DEFAULT 0, update_pref VARCHAR(64) DEFAULT 'daily', min_days_between_msg MEDIUMINT DEFAULT 0, last_date_messaged DATE)")
         db.run(connection,"Create table "+table_reddit+" (id VARCHAR(64) NOT NULL UNIQUE, location MEDIUMINT DEFAULT 0, update_pref VARCHAR(64) DEFAULT 'daily', min_days_between_msg MEDIUMINT DEFAULT 0, last_date_messaged DATE)")
         db.commit(connection)
@@ -49,7 +53,7 @@ class userbase:
         return data.fips_from_text(location)
 
     def new_user(id,location="USA",update_pref="daily",min_days_bewtween_msg=0,platform="twitter"): #for location by county & state, set location to "{County}, {State}".format(). ID is string.
-        connection = db.create_connection(data.sql_addr, data.sql_user, data.sql_pass, data.db_name)
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass, data.db_name)
         fips = data.fips_from_text(location)
         db.insert(connection,userbase.table_from_param(platform),formatt=["id","location","update_pref","min_days_between_msg"],valueset=[id,fips,update_pref,min_days_bewtween_msg])
         db.commit(connection)
@@ -59,13 +63,13 @@ class userbase:
         #        print("[UPLOADERROR] error uploading, probable duplicate entry with id ",id)
 
     def set(id,field,new_value,platform="twitter"): #set attribute of user
-        connection = db.create_connection(data.sql_addr, data.sql_user, data.sql_pass, data.db_name)
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass, data.db_name)
         db.update(connection,userbase.table_from_param(platform),field,new_value,condition=f"id = '{id}'")
         db.commit(connection)
         connection.close()
 
     def get(id,platform="twitter"): #get dict of all user data
-        connection = db.create_connection(data.sql_addr, data.sql_user, data.sql_pass, data.db_name)
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass, data.db_name)
         udata_raw = db.query(connection,userbase.table_from_param(platform),"*",condition=f"id = '{id}'")
         cnames = db.get_column_names(connection,userbase.table_from_param(platform))
         connection.close()
@@ -78,7 +82,7 @@ class userbase:
             return None
 
     def if_user_exists(id,platform="twitter"):
-        connection = db.create_connection(data.sql_addr, data.sql_user, data.sql_pass, data.db_name)
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass, data.db_name)
         r = db.query(connection,userbase.table_from_param(platform),"id",condition=f"id = '{id}'")
         connnection.close()
         if len(r) > 0:
@@ -86,7 +90,7 @@ class userbase:
         return False
 
     def remove_user(id,platform="twitter"):
-        connection = db.create_connection(data.sql_addr, data.sql_user, data.sql_pass, data.db_name)
+        connection = db.create_connection(data.sql_addr, userbase.sql_user, userbase.sql_pass, data.db_name)
         db.delete(connection,userbase.table_from_param(platform),f"id = '{id}'")
         db.commit(connection)
         connection.close()
