3153229c8c2274d8d910730dc8df225842963966
Andy Anguiano <aanguiano@chapman.edu>
2020-05-02 23:08:31 -0700

    added dm support

--- myBot.py
+++ myBot.py
@@ -12,9 +12,8 @@ access_token_secret="CFCmMQEwR223jLyO9kfvwoi48cZydE5PBRmS7PSgaPPwz"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-#allMembers = {}
 
 FILE_NAME = 'lastSeenId.txt'
 #keeps track of the last seen id from all the mentions
@@ -29,6 +28,18 @@ def store_last_seen_id(last_seen_id, file_name):
     f_write.write(str(last_seen_id))
     f_write.close()
     return
+
+def retrieve_last_dm():
+    f_read = open('lastSeenDM.txt', 'r')
+    count = int(f_read.read())
+    f_read.close()
+    return count
+
+def new_last_dm(count):
+    f_write = open('lastSeenDM.txt', 'w')
+    f_write.write(str(count))
+    f_write.close()
+    return count
 
 def reply_to_tweets():
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
@@ -62,22 +73,21 @@ def string_to_send(fips):
     info_string = "Total Number of Cases: " + str(total_cases) + "\n" + "Total Number of Deaths: " + str(total_deaths) + "\n" + "New Cases Today: " + str(new_cases) + "\n" +"New Deaths Today: " + str(new_deaths)
     return info_string
 
-
 #send dm to all followers
 def send_direct_messages():
-    subscriberList = api.followers_ids(1249410623160475649)
-    for sub in subscriberList:
-        if userbase.if_user_exists(sub):
-            #HOW DO I GET FIPS
-            fips =
-            information = string_to_send(fips)
-            api.send_direct_message(id, information)
+    messageData = api.list_direct_messages()
+    for words in messageData:
+        if words.message_create.get(u'sender_id') != '1249410623160475649':
+            id = words.message_create.get(u'sender_id')
+            if userbase.if_user_exists(id):
+                dict = userbase.get(id)
+                fips = dict[location]
+                information = string_to_send(fips)
+                api.send_direct_message(id, information)
 
 #returns if need to send to dm right away
 def add_dictionary(id, message):
     fips = userBase.fips_from_text(message)
-    #if id not in allMembers:
-    #    allMembers[id] = fips
     if not userbase.if_user_exists(id):
         userbase.new_user(id, location=fips)
         return True
@@ -86,23 +96,36 @@ def add_dictionary(id, message):
 #gets all recieved dms text
 def get_all_received():
     messageData = api.list_direct_messages()
-    for words in messageData:
+    count = retrieve_last_dm()
+    inCount = 0
+    for words in reversed(messageData):
         if words.message_create.get(u'sender_id') != '1249410623160475649':
-            text = words.message_create.get(u'message_data').get(u'text')
-            id = words.message_create.get(u'sender_id')
-            text = text.lower()
-            if text == "stop":
-                userbase.remove_user(id)
-                break
-            in_bool = add_dictionary(id, text)
-            if in_bool:
-                fips = userbase.fips_from_text(text)
-                information = string_to_send(fips)
-                api.send_direct_message(id, information)
+            if inCount >= count:
+                text = words.message_create.get(u'message_data').get(u'text')
+                id = words.message_create.get(u'sender_id')
+                text = text.lower()
+                text = text.lower()
+                if "stop" in text:
+                    userbase.remove_user(id)
+                elif "subscribe " in text:
+                    text.replace("subscribe ", '')
+                    add_dictionary(id, text)
+                    api.send_direct_message(id, "We will keep you updated, reply 'stop' to quit")
+                    fips = userbase.fips_from_text(text)
+                    information = string_to_send(fips)
+                    api.send_direct_message(id, information)
+                else:
+                    fips = userbase.fips_from_text(text)
+                    information = string_to_send(fips)
+                    api.send_direct_message(id, information)
+                inCount += 1
+            else:
+                inCount += 1
+    new_last_dm(inCount)
+
 
 if __name__ == '__main__':
     reply_to_tweets()
     get_all_received()
-    #HOW TO RUN THIS EVERY DAY
     #need to only run this everyday
     #send_direct_messages()
